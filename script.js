8c270709bd343b2d3ef45fd8c9465835332e642e
Allangui <75880445+Allangui@users.noreply.github.com>
2021-04-25 15:00:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -39,21 +39,31 @@ let pledge = document.querySelectorAll('.input-pledge');
 let submit = document.querySelectorAll('.btn-submit')
 let success = document.querySelector('.card-success');
 let btnSuccess = document.querySelector('.btn-success');
+let card = document.querySelectorAll('.card-pop');
+let btnReward = document.querySelectorAll('.btn-reward');
+
+
 
 for ( let i =0;i< check.length ; i++){
 zone[i].onclick = function(){
   if (check[i].checked ===false) {
     check[i].checked =true
     selected[i].classList.toggle('hide')
+    card[i].classList.toggle('card-selected');
+
 
   } else {
     check[i].checked = false;
     selected[i].classList.toggle('hide')
+    card[i].classList.toggle('card-selected');
+
 
   }
 }
 check[i].onchange = function(){
 selected[i].classList.toggle('hide')
+card[i].classList.toggle('card-selected');
+
   }
 }
 
@@ -61,13 +71,28 @@ for(let i =0 ; i< submit.length ; i++){
   submit[i].onclick = function(){
     for(let i =0;i<check.length ; i++){
       check[i].checked = false;
+      card[i].classList.remove('card-selected');
       selected[i].classList.add('hide');
+
     }
     success.classList.remove('hide');
     popup.classList.add('hide');
 
   }
 }
+
+for( let i=0; i<btnReward.length ; i++){
+  btnReward[i].onclick = function(){
+  popup.classList.toggle('hide');
+  
+  check[(i+1)].checked = true;
+  selected[(i+1)].classList.remove('hide')
+  card[(i+1)].classList.add('card-selected');
+  console.log(i)
+ };
+
+}
+
 
 btnSuccess.onclick = function(){
   success.classList.add('hide');
